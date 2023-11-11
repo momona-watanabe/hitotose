28f89e67317071e8f74d62c57608ee0034087c41
momona-watanabe <140260256+momona-watanabe@users.noreply.github.com>
2023-11-11 22:40:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,51 @@ $("#g-nav a").click(function () {
     $("#g-nav").removeClass('panelactive');
     $(".circle-bg").removeClass('circleactive');
 });
+
+
+
+
+
+
+
+
+
+
+//slick
+$('.slider').slick({
+  fade:true,
+  autoplay:true,
+  autoplaySpeed:3000,
+  speed:1000,
+  infinite:true,
+  slideToShow:1,
+  slideToScroll:1,
+  arrows:false,
+  pauseOnFocus:false,
+  pauseOnHover:false,
+  pauseOnDotsHover:false,
+});
+
+
+
+
+
+
+
+//blur
+function fadeAnime1(){
+  $('.blurTrigger').each(function(){
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windouHeight = $(window).height();
+    if (scroll >= elemPos - windouHeight){
+      $(this).addClass('blur');
+    }else{
+      $(this).removeClass('blur');
+    }
+  });
+}
+
+$(window).scroll(function (){
+  fadeAnime1();
+});
