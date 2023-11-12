2fbae0ee53be4d13e58318c74b803513f8f099bd
momona-watanabe <140260256+momona-watanabe@users.noreply.github.com>
2023-11-12 22:33:42 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -55,3 +55,18 @@ function fadeAnime1(){
 $(window).scroll(function (){
   fadeAnime1();
 });
+
+
+
+
+
+//loading
+$(window).on('load',function(){
+  $("#splash_logo").delay(1200).fadeOut('slow');
+  $("#splash").delay(1500).fadeOut('slow',function(){
+  $('body').addClass('appear');
+  });
+  
+  $('.splashbg1').on('animationend', function() { 
+  });
+  });
